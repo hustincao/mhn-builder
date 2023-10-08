a2a805a18a53d4660806d5fdaa91eba18635a843
hustincao <40309113+hustincao@users.noreply.github.com>
2023-10-08 18:13:21 -0400

    Add info tooltip to stats and skill

--- src/routes/stats.svelte
+++ src/routes/stats.svelte
@@ -1,5 +1,6 @@
 <script>
   import Dropdown from "$lib/components/dropdown.svelte";
+  import Tooltip from "$lib/components/tooltip.svelte";
 
   // Component that displays the stats for a set.
   export let set;
@@ -172,7 +173,31 @@
       </div>
     </Dropdown>
 
-    <p class="text-center font-bold text-lg grow">Stats</p>
+    <p class="font-bold text-lg grow flex items-center justify-center gap-x-2">
+      Stats
+      {#if !isHidden}
+        <Tooltip>
+          <svg
+            slot="label"
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 512 512"
+            ><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
+            /></svg
+          >
+          <div slot="description">
+            <p>
+              The only skills added onto stats are <span class="italic"
+                >Health Boost, Defense Boost, Attack Boost, Critical Eye, Poison
+                Attack, Paralysis Attack, Fire Attack, Water Attack, Thunder
+                Attack, and Ice Attack.</span
+              >
+            </p>
+          </div>
+        </Tooltip>
+      {/if}
+    </p>
     <button
       class="p-1 rounded-md hover:bg-slate-400"
       on:click={() => {
